9ad83103708f79524dbe326857773c75326b076e
Tony Dixon <tony.dixon@borwell.com>
2022-06-29 12:10:01 +0100

    sets default dimension values

--- src/main/java/com/borwell/paint/calculator/model/Dimensions.java
+++ src/main/java/com/borwell/paint/calculator/model/Dimensions.java
@@ -8,6 +8,15 @@ public class Dimensions
     private double length;
     private double width;
     private double height;
+    private double coverage;
+
+    public Dimensions()
+    {
+        this.coverage = 1.0;
+        this.length = 1.0;
+        this.width = 1.0;
+        this.height = 1.0;
+    }
 
     public double getLength()
     {
@@ -37,5 +46,15 @@ public class Dimensions
     public void setHeight(double height)
     {
         this.height = height;
+    }
+
+    public double getCoverage()
+    {
+        return coverage;
+    }
+
+    public void setCoverage(double coverage)
+    {
+        this.coverage = coverage;
     }
 }
