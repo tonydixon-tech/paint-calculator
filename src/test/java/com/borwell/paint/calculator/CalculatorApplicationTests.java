62e697ec064dfa6790176e5efe53c66158b2dc9b
Tony Dixon <tony.dixon@borwell.com>
2022-07-28 11:29:35 +0100

    tests that context loads

--- src/test/java/com/borwell/paint/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/borwell/paint/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,17 @@
 package com.borwell.paint.calculator;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class CalculatorApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+    assertNotNull(context);
 	}
 
 }
